3c4c498a25418866744f37ccf58a475ebc3a747f
spasztor <spasztor@goldsmithengineering.com>
2014-07-01 11:32:15 -0700

    Implemented shutil.copy()

--- StartCorrsepond2013/StartCorrsepond2013/StartCorrespond2013.py
+++ StartCorrsepond2013/StartCorrsepond2013/StartCorrespond2013.py
@@ -2,22 +2,23 @@ import shutil
 import sys
 import os
 
-arg = ['1','2','3','4','5']
+arg = ['1','2','3','4','5','6']
 srcBase = "H:/admin/DATABASE/FrontEnd/2013/"
 srcAdd = {arg[0] : "ARCHFE2013.accdb", arg[1] : "corresfe2013.accdb", 
           arg[2] : "DCNAMES2013.accdb", arg[3] : "inventfe2013.accdb", 
-          arg[4] : "phone2013.accdb"}
+          arg[4] : "phone2013.accdb", arg[5] : "isdbfe2013.accdb"}
 
-tar = "C:/Access/HGGMain/2013/"
+dest = "C:/Access/HGGMain/2013/"
 
-
-if (len(sys.argv)) > 1 and (sys.argv[0] == '1' or sys.argv[0] == '2' or sys.argv[0] == '3' 
-                                               or sys.argv[0] == '4' or sys.argv[0] == '5'):
-    path = os.path.join(srcBase, srcAdd[sys.argv[0]])
-    print(path)
+if (len(sys.argv)) > 1 and (sys.argv[1] == arg[0] or sys.argv[1] == arg[1] or sys.argv[1] == arg[2] 
+                                                  or sys.argv[1] == arg[3] or sys.argv[1] == arg[4]
+                                                  or sys.argv[1] == arg[5]):
+    srcPath = os.path.join(srcBase, srcAdd[sys.argv[0]])
+    shutil.copy(srcPath,dest)
 else:
     for item in arg:
-        print("Hello World! And:", srcAdd[item])
+        srcPath = os.path.join(srcBase, srcAdd[item])
+        shutil.copy(srcPath,dest)
 
 input("press enter to exit:")
  
